c4251dcc7c20d9665f165cf8dc0c402b42ca3d89
Abhishek Shelke <abhishelke297127@gmail.com>
2024-06-02 17:58:51 +0530

    lecture-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductComponent } from './product/product.component';
+import { StudentModule } from './student/student.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProductComponent],
+  imports: [RouterOutlet, ProductComponent, StudentModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'myApp';
-  fNAme="abhishek";
-  a=2;
-  b=5;
-  add(a:number,b:number){
-    return a+b;
+  fNAme = 'abhishek';
+  a = 2;
+  b = 5;
+
+  add(a: number, b: number): number {
+    return a + b;
   }
+
+  heading = 'this is heading';
+  studentList = [
+    { id: 1, name: 'abhishek', age: 25 },
+    { id: 2, name: 'rahul', age: 30 },
+    { id: 3, name: 'raj', age: 35 }
+  ];
 }
